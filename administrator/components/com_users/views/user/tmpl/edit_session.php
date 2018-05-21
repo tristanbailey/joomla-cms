107531ad94ee770bb862c208de9e732c7cb9fcac
Tristan Bailey <tristanbailey@gmail.com>
2018-05-21 18:53:56 +0100

    remove commends and add tabs

--- administrator/components/com_users/views/user/tmpl/edit_session.php
+++ administrator/components/com_users/views/user/tmpl/edit_session.php
@@ -11,14 +11,13 @@ defined('_JEXEC') or die;
 
 if ($this->item->id !== 0)
 {
-    // $user_id = $jinput->get('id');
-    JHtml::script('com_users/sessionend.js', false, true);
-    JFactory::getDocument()->addScriptDeclaration('
-        var endsession_url = "' . addslashes(JUri::base()) . 'index.php?option=com_users&task=user.endSession&format=json&' . JSession::getFormToken() . '=1";
-        var user_id = ' . JFactory::getApplication()->input->get('id', 0, 'int') . ';
-     ');
+	JHtml::script('com_users/sessionend.js', false, true);
+	JFactory::getDocument()->addScriptDeclaration('
+		var endsession_url = "' . addslashes(JUri::base()) . 'index.php?option=com_users&task=user.endSession&format=json&' . JSession::getFormToken() . '=1";
+		var user_id = ' . JFactory::getApplication()->input->get('id', 0, 'int') . ';
+	');
 
-    echo '<button type="button" class="btn btn-small" id="end_session">
-            <span>' . JText::_('COM_USERS_END_SESSION_ACTION_BUTTON') . '</span>
-        </button>';
+	echo '<button type="button" class="btn btn-small" id="end_session">
+		<span>' . JText::_('COM_USERS_END_SESSION_ACTION_BUTTON') . '</span>
+	</button>';
 }
